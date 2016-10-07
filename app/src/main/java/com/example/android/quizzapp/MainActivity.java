d8ce0d2f14f8cf29969dffa2f30d2fb768987a79
MechaEdgar <ee1206@hotmail.com>
2016-10-07 13:47:28 -0500

    New Quiz Project.

---
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.quizzapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+public int correctAnswer = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void CheckAnswers(int correctAnswer){
+      // TextView check = findViewById(R.id.);
+    }
+
+}
