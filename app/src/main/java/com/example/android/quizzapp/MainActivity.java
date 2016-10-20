4396be9c68983c4bf12ac6a5d91ec9a468019bb9
MechaEdgar <ee1206@hotmail.com>
2016-10-20 17:22:23 -0500

    Layout changes.

--- app/src/main/java/com/example/android/quizzapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizzapp/MainActivity.java
@@ -2,18 +2,20 @@ package com.example.android.quizzapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
+import static android.R.attr.id;
+
 public class MainActivity extends AppCompatActivity {
-public int correctAnswer = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void CheckAnswers(int correctAnswer){
-      // TextView check = findViewById(R.id.);
     }
 
-}
+
